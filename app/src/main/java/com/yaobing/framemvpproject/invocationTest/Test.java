5bcc256071222aaedf27f613999601353347ad13
yaobing <1124507487@qq.com>
2021-04-01 09:38:22 +0800

    更新gradle版本，用于测试jenkins打包

---
+++ app/src/main/java/com/yaobing/framemvpproject/invocationTest/Test.java
@@ -0,0 +1,12 @@
+package com.yaobing.framemvpproject.invocationTest;
+
+/**
+ * @author : yaobing
+ * @date : 2021/3/19 11:07
+ * @desc :
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("zxcvb");
+    }
+}
