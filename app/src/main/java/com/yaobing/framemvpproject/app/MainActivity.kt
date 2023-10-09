065b0a6f2e05f2dcf81ce127f18ee1d714514721
890063 <890063@leapmotor.com>
2023-10-09 11:16:47 +0800

    测试动态代理，优化UI和配置项抽取到rootProject

--- app/src/main/java/com/yaobing/framemvpproject/app/MainActivity.kt
+++ app/src/main/java/com/yaobing/framemvpproject/app/MainActivity.kt
@@ -25,13 +25,13 @@ class MainActivity : BaseControllerActivity() , GithubRepoContract.View{
         super.onCreate(savedInstanceState)
         bt_all.setOnClickListener {
             //为了测试动态代理方便，先注释掉
-            presenterRouter.create(GithubRepoAPI:: class.java).getAllRepoByName("MCKRGF")
+//            presenterRouter.create(GithubRepoAPI:: class.java).getAllRepoByName("MCKRGF")
 
-//            val testRouterA = TestRouter()
-//            val obj = AAA()
-//            testRouterA.register(obj)
-//            val a = testRouterA.create(TestAPI:: class.java)
-//            a.testFun("AAA")
+            val testRouterA = TestRouter()
+            val obj = AAA()
+            testRouterA.register(obj)
+            val a = testRouterA.create(TestAPI:: class.java)
+            a.testFun("AAA")
 
 
         }
@@ -44,7 +44,7 @@ class MainActivity : BaseControllerActivity() , GithubRepoContract.View{
             b.testFun("BBB")
 
 //为了测试动态代理方便，先注释掉
-            IntentRouter.go(this, "asdf")
+//            IntentRouter.go(this, "asdf")
         }
         initWebView()
     }
